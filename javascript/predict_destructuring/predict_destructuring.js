034135ef8ca32b3b6a6dfd4ef411c29c3314585b
aravindrs44 <aravindrs@hotmail.com>
2021-07-03 16:21:16 -0700

    initial commit

---
+++ javascript/predict_destructuring/predict_destructuring.js
@@ -0,0 +1,17 @@
+// Problem 1
+// No error
+
+// Problem 2
+// var name does not exist
+
+// Problem 3
+// Compilation Error: hashedpassword
+
+// Problem 4
+// Output: False, True
+
+// Problem 5
+// Output:
+// [1, 5, 1, 8, 3, 3]
+// 1
+// 5
