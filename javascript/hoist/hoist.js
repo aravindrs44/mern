9a2c6b9a3edc81819c4ed4800149e46218e84d3a
aravindrs44 <aravindrs@hotmail.com>
2021-07-04 12:20:31 -0700

    initial commit

--- javascript/hoist/hoist.js
+++ javascript/hoist/hoist.js
@@ -1,11 +1,16 @@
+// Problem 1 //
 console.log(hello);
 var hello = 'world';
 
-// var hello = 'world';
-// console.log(hello);
-// expected:
-// undefined
+/*
+var hello;
+console.log(hello);
+hello = 'world';
+expected:
+undefined
+*/
 
+//Problem 2//
 var needle = 'haystack';
 test();
 function test(){
@@ -13,102 +18,126 @@ function test(){
     console.log(needle);
 }
 
-// function test(){
-//     var needle = 'magnet';
-//     console.log(needle);
-// }
-// test();
-// var needle = 'haystack';
-// expected:
-// magnet
+/*
+var needle;
+needle = 'haystack';
+function test(){
+    var needle;
+    needle = 'magnet';
+    console.log(needle);
+}
+test();
+expected:
+magnet
+*/
 
+// Problem 3 //
 var brendan = 'super cool';
 function print(){
-    brendan = 'only okay';
-    console.log(brendan);
+    brendan = 'only okay';
+    console.log(brendan);
 }
 console.log(brendan);
 
-// var brendan = 'super cool';
-// function print(){
-//     brendan = 'only okay';
-//     console.log(brendan);
-// }
-// console.log(brendan);
-// expected:
-// super cool
+/*
+var brendan;
+brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+expected:
+super cool
+*/
 
+// Problem 4 //
 var food = 'chicken';
 console.log(food);
 eat();
 function eat(){
-    food = 'half-chicken';
-    console.log(food);
-    var food = 'gone';
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
 }
 
-// var food = 'chicken';
-// console.log(food);
-// function eat(){
-//     food = 'half-chicken';
-//     console.log(food);
-//     var food = 'gone';
-// }
-// eat();
-// expected:
-// chicken
-// half-chicken
+/*
+var food;
+food = 'chicken';
+console.log(food);
+function eat(){
+    var food;
+    food = 'half-chicken';
+    console.log(food);
+    food = 'gone';
+}
+eat();
+expected:
+chicken
+half-chicken
+*/
 
+// Problem 5 //
 mean();
 console.log(food);
 var mean = function() {
-    food = "chicken";
-    console.log(food);
-    var food = "fish";
-    console.log(food);
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
 }
 console.log(food);
 
-// var mean = function() {
-//     food = "chicken";
-//     console.log(food);
-//     var food = "fish";
-//     console.log(food);
-// }
-// mean();
-// console.log(food);
-// console.log(food);
+/*
+var mean;
+mean();
+console.log(food);
+mean = function() {
+    var food;
+    food = "chicken";
+    console.log(food);
+    food = "fish";
+    console.log(food);
+}
+console.log(food);
+*/
 
-// expected: function doesn't have a name
+// expected: mean is initialized but has no value so it returns a mean is not a function error
 
+// Problem 6 //
 console.log(genre);
 var genre = "disco";
 rewind();
 function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
 }
 console.log(genre);
 
-// console.log(genre);
-// var genre = "disco";
-// function rewind() {
-//     genre = "rock";
-//     console.log(genre);
-//     var genre = "r&b";
-//     console.log(genre);
-// }
-// rewind();
-// console.log(genre);
+/*
+var genre;
+console.log(genre);
+genre = "disco";
+function rewind() {
+    var genre;
+    genre = "rock";
+    console.log(genre);
+    genre = "r&b";
+    console.log(genre);
+}
+rewind();
+console.log(genre);
 
-// expected:
-// undefined
-// rock
-// r&b
-// r&b
+Expected:
+Undefined
+rock
+r&b
+r&b
+*/
 
+// Problem 7
 dojo = "san jose";
 console.log(dojo);
 learn();
@@ -120,23 +149,27 @@ function learn() {
 }
 console.log(dojo);
 
-// dojo = "san jose";
-// console.log(dojo);
-// function learn() {
-//     dojo = "seattle";
-//     console.log(dojo);
-//     var dojo = "burbank";
-//     console.log(dojo);
-// }
-// learn();
-// console.log(dojo);
+/*
+dojo = "san jose";
+console.log(dojo);
+function learn() {
+    var dojo;
+    dojo = "seattle";
+    console.log(dojo);
+    dojo = "burbank";
+    console.log(dojo);
+}
+learn();
+console.log(dojo);
 
-// expected:
-// san jose
-// seattle
-// burbank
-// san jose
+expected:
+san jose
+seattle
+burbank
+san jose
+*/
 
+// Problem 8
 console.log(makeDojo("Chicago", 65));
 console.log(makeDojo("Berkeley", 0));
 function makeDojo(name, students){
@@ -152,20 +185,22 @@ function makeDojo(name, students){
     return dojo;
 }
 
-// function makeDojo(name, students){
-//     const dojo = {};
-//     dojo.name = name;
-//     dojo.students = students;
-//     if(dojo.students > 50){
-//         dojo.hiring = true;
-//     }
-//     else if(dojo.students <= 0){
-//         dojo = "closed for now";
-//     }
-//     return dojo;
-// }
-// console.log(makeDojo("Chicago", 65));
-// console.log(makeDojo("Berkeley", 0));
+/*
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo;
+    dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    } else if(dojo.students <= 0) {
+        dojo = "closed for now";
+    }
+    return dojo;
+}
 
-// expected:
-// error
+Expected:
+Parse error dojo is a const and cannot be reassigned
+*/
