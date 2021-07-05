42ce1e45080e555f12d864f9f6594b06e83c9a83
aravindrs44 <aravindrs@hotmail.com>
2021-07-04 21:53:30 -0700

    initial commit

---
+++ javascript/oop/its_time_to_duel/its_time_to_duel.js
@@ -0,0 +1,43 @@
+class Card  {
+    constructor(name, cost)  {
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+class Unit extends Card  {
+    constructor(name, cost, power, res)  {
+        super(name, cost);
+        this.power = power;
+        this.res = res;
+    }
+    attack(target)  {
+        target.res -= this.power;
+    }
+}
+
+class Effect extends Card  {
+    constructor(name, cost, text, stat, mag)  {
+        super(name, cost);
+        this.text = text;
+        this.stat = stat;
+        this.mag = mag;
+    }
+
+    play (target)  {
+        if (target instanceof Unit)  {
+            this.stat + this.mag;
+            console.log(this.text);
+        }
+    }
+}
+
+red_belt_ninja = new Unit("Red Belt Ninja", 3, 3, 4);
+hard_algorithm = new Effect("Hard Algorithm", 2, "Increases target's resilience by 3", red_belt_ninja.res, 3);
+hard_algorithm.play(red_belt_ninja);
+black_belt_ninja = new Unit("Black Belt Ninja", 4, 5, 4);
+unhandled_promise_rejection = new Effect("Unhandled Promise Rejection", 1, "Reduces target's power by 2", red_belt_ninja.power, -2);
+unhandled_promise_rejection.play(red_belt_ninja);
+pair_programming = new Effect("Pair Programming", 3, "Increases target's power by 2", red_belt_ninja.power, 2);
+pair_programming.play(red_belt_ninja);
+
